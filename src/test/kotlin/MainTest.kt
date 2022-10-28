511c34a58766fd768bb40ee60955cd3e410af436
Francisco <fsousa.jpa@gmail.com>
2022-10-28 13:11:03 -0300

    iniciando projeto

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.*
+
+/*
+* assertTrue
+* assertFalse
+* assertAll
+* assertIquals
+* annotations @
+* -Desabilitar teste
+* -Falhar teste caso a implementacao nao esteja feita
+* */
+
+class MainTest {
+
+    @Test
+    @DisplayName("Teste metodo xxoo")
+    fun testCountXO() {
+        Assertions.assertAll(
+            {Assertions.assertTrue(countXO("xxooxo"))},
+            {Assertions.assertTrue(countXO("xxxooo"))},
+        )
+    }
+
+    @Test
+    @Disabled
+    fun naoImplementado() {
+
+    }
+
+    @Test
+    fun vaiFalhar() {
+        fail("Não posso terminar os testes sem esse metodo")
+    }
+}
